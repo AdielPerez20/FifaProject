4a1838dece0145fa4209dc9534c2cbfbd7563639
AdielPerez20 <adielperez20@gmail.com>
2018-10-18 18:05:47 +0300

    ConectDB

--- events/server.js
+++ events/server.js
@@ -21,6 +21,29 @@ app.use('/js', express.static(__dirname + '/node_modules/tether/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
+//Connect to DataBase
+const con =mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "FIFAProject"
+});
+
+//Global site title and base url
+const siteTitle = "FIFA Project";
+const baseURL = "http://localhost:4000/"
+
+app.get('/', function (req, res){
+    con.query("SELECT * FROM Leagues ORDER BY idLeagues", function(err, result){
+        res.render('pages/index',{
+            siteTitle : siteTitle,
+            pageTitle : "Event list",
+            items : result
+        });
+    });
+
+});
+
 //Connect to Server
 var server = app.listen(4000,function(){
     console.log("Server started on port 4000..");
